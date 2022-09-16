1a84a6256f8626efa48ab7d517bb7cc91705bb17
Akp-repo <108732858+Akp-repo@users.noreply.github.com>
2022-09-16 15:48:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@
         }
       }
     }
+
+
+ # An example resource that does nothing.
+    resource "null_resource" "example" {
+      triggers = {
+        value = "A example resource that does nothing!"
+      }
+    }
